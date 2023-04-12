49f541fff0fdfe051ce13514129c897992d7fefd
mikcrophone <69864260+mikcrophone@users.noreply.github.com>
2023-04-12 13:05:16 -0400

    Implement frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,60 +1,25 @@
-// import React from "react";
-// function MyButton() {
-//   return (
-//     <button>
-//       I'm a button
-//     </button>
-//   );
-// }
-
-// export default function MyApp() {
-//   return (
-//     <div>
-//       <h1>Welcome to my app</h1>
-//       <MyButton />
-//     </div>
-//   );
-// }
 
 import React, { useState } from 'react';
-// import './App.css';
-// import Display from './Display.js';
+import {BrowserRouter, Route, Link, Routes, useLocation} from 'react-router-dom';
+
+import Signup from './Signup';
 
 export default function App() {
-  const [name, setName] = useState('Julie');
-  const [age, setAge] = useState('aging well');
-  const [weight, setWeight] = useState(180);
-  //var state = { name: 'Julie', age: 'aging well', weight: 180 };
-
-  const clicker = (name) => {
-    setName(name);
-    if (name.localeCompare('Daniel') === 0) {
-      setAge('TOO OLD!!');
-      setWeight(200);
-    } else {
-      setAge('not as old as Daniel');
-      setWeight(150);
-    }
-  }
-
-  // const renderDisplay = (data) => {
-  //   return <Display stats={data} />;
-  // }
-
-  const renderButton = () => {
-    return (
-      <button onClick={() => clicker(document.getElementById('fname').value)}>
-        {name}
-      </button>
-    );
-  }
-
-  const state = { name: name, age: age, weight: weight };
   return (
     <div>
-      <div>Enter a name: <input type="text" id="fname" name="fname" /></div>
-      {/* <div>{renderDisplay(state)}</div> */}
-      <div>{renderButton("Button")}</div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/:clueURLStem" element={<Signup></Signup>}></Route>
+          <Route path="/" element={<div>Leaderboard goes here</div>}/>
+          <Route path="/leaderboard" element={<div>Leaderboard goes here</div>}/>
+        </Routes>
+        
+      </BrowserRouter>
+      
+      
     </div>
-  );
+    
+    
+  )
 }
