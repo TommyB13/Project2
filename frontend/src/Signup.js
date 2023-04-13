f5f20a263ff5e4ff0f732f5e0f8d7a40a1909e80
mikcrophone <69864260+mikcrophone@users.noreply.github.com>
2023-04-13 16:41:51 -0400

    fixing urlstem for clue

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,22 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {useParams} from 'react-router-dom';
+import axios from 'axios';
 
 import './signup.css'
 
 function Signup() {
+  const [cluedata, setClueData] = useState({data:{}});
+  let clueExists = cluedata.data !== undefined;
 
-    let {clueUrlStem} = useParams();
-return (
-    <div>
-    <h1>Team Registration</h1>
-    <p>{clueUrlStem}</p>
-    <form action="#" method="post">
-      <label for="team-name">Team Name:</label>
-      <input type="text" id="team-name" name="team-name" required></input>
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-)
+  let {clueURLStem} = useParams();
+
+  console.log(clueURLStem);
+    useEffect(() => {
+      
+      async function getResults() {
+        await axios('http://localhost:2400/api/clues/' + clueURLStem).then( results => {
+          setClueData({data: results.data})
+        
+        }).catch(err => {
+          setClueData({});
+        });
+        
+        
+      }
+      getResults()
+    }, []);
+    
+    
+    if(clueExists) {
+        
+        return(
+        <div>
+
+        <h1>Team Registration</h1>
+        <form action="#" method="post">
+          <label for="team-name">Team Name:</label>
+          <input type="text" id="team-name" name="team-name" required></input>
+          <button type="submit">Submit</button>
+        </form>
+        
+        </div>)
+      }
+
+      else {
+
+        return(
+
+          <p> This clue does not exist</p>
+
+        )
+
+      }
+
+  
+
 
 
 }
